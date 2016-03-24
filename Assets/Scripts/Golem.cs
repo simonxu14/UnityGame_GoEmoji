78bd3842d1e4e395a37e64cf31f9469c054fc141
Xinlong Xu <simonxu14@gmail.com>
2016-03-24 09:46:44 -0700

    v2

---
+++ Assets/Scripts/Golem.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Golem : MonoBehaviour {
+
+	private Animator animator;
+	public Vector2 target;
+	private Vector2 start;
+	private Vector2 curTarget;
+	private bool flag;
+	private Rigidbody2D rb2d;
+	private Vector2 flip;
+
+	private int health;
+	public GameObject claw;
+	private int time;
+	private bool isDie;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+		start.x = transform.position.x;
+		start.y = transform.position.y;
+		flip = transform.localScale;
+		curTarget = target;
+		flag = true;
+		health = 5000;
+		time = 0;
+		isDie = false;
+	}
+
+	public void Attack(bool temp) {
+		if (temp) {
+			animator.SetTrigger ("GolemWalkToAttack");
+			claw.SetActive (true);
+			claw.transform.position = transform.position;
+		}
+		if (!temp) {
+			animator.SetTrigger ("GolemAttackToWalk");
+		}
+	}
+
+	public void takeDmg(int val){
+		if (health > val) {
+			health -= val;
+		} else {
+			Die();
+		}
+	}
+
+	public void Die() {
+		animator.SetTrigger ("GolemDie");
+		isDie = true;
+		//		gameObject.SetActive(false);
+	}
+
+	void Update () {
+		if (isDie == true) {
+			time += 1;
+			if (time == 100) {
+				gameObject.SetActive (false);
+			}
+		}
+
+		animator.SetTrigger ("GolemAttackToWalk");
+
+		if (reach ()) {
+			if (flag) {
+				curTarget = start;
+				flip.x *= -1;
+				transform.localScale = flip;
+			}
+			else {
+				curTarget = target;
+				flip.x *= -1;
+				transform.localScale = flip;
+			}
+			flag = !flag;
+		}
+	}
+
+	void FixedUpdate () {
+		transform.localRotation = Quaternion.identity;
+		float moveH = (curTarget.x - transform.position.x);
+		float moveV = (curTarget.y - transform.position.y);
+		Vector2 movement = new Vector2 (moveH, moveV);
+		movement.Normalize ();
+		if(rb2d.velocity.magnitude<0.5)
+			rb2d.AddForce (movement);
+	}
+
+	bool reach() {
+		return (Mathf.Abs(transform.position.x - curTarget.x)<0.1)&&(Mathf.Abs(transform.position.y - curTarget.y)<0.1);
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject shoot;
+	public GameObject player;
+	private int timer;
+
+	void Start () {
+		Random.seed = (int)(transform.position.x*100);
+		int n = Random.Range (0, 50);
+		timer = 100 + n;
+	}
+
+	void Update () {
+		if (timer > 90) {
+			timer--;
+		} else if (timer == 90) {
+			shoot.SetActive (true);
+			shoot.transform.position = transform.position;
+			Quaternion rotation = Quaternion.LookRotation (player.transform.position - transform.position, transform.TransformDirection (Vector3.forward));
+			shoot.transform.rotation = new Quaternion (0, 0, rotation.z, rotation.w);
+//			shoot.transform.localScale = new Vector3 (0.1f, 0.01f, 1);
+			timer--;
+		} else if (timer > 0) {
+			shoot.transform.position = transform.position;
+			Quaternion rotation = Quaternion.LookRotation (player.transform.position - transform.position, transform.TransformDirection (Vector3.forward));
+			shoot.transform.rotation = new Quaternion (0, 0, rotation.z, rotation.w);
+//			shoot.transform.localScale += new Vector3 (0.001f, 0.001f, 0);
+			timer--;
+		} else if (timer == 0) {
+			float vX = player.transform.position.x - shoot.transform.position.x;
+			float vY = player.transform.position.y - shoot.transform.position.y;
+			Vector2 movement = new Vector2 (vX, vY);
+			movement.Normalize ();
+			shoot.GetComponent<Rigidbody2D> ().velocity = movement;
+			timer--;
+		} else if (!shoot.activeSelf) {
+			Random.seed = (int)(transform.position.x * 100);
+			int n = Random.Range (0, 50);
+			timer = 100 + n;
+		} else if (timer == -180) {
+			shoot.SetActive (false);
+		} else if (timer < 0) {
+			timer--;
+		}
+	}
+}
